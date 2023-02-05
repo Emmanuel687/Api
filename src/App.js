e42f5c569a94c6e1d6337fbf5940d482ec7f7540
Emmanuel Koech <93251478+Emmanuel687@users.noreply.github.com>
2023-02-05 20:51:19 +0300

    Created Footer Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./component/layout/Navbar";
 
 function App() {
   return (
-    <div>
-     <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </div>
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar></Navbar>
+
+        <main>content</main>
+
+      </div>
+    </Router>
   );
 }
 
