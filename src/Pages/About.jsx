f2ef970fddf690b97bc55588a09dd9ceea620259
Emmanuel Koech <93251478+Emmanuel687@users.noreply.github.com>
2023-02-05 21:53:21 +0300

    Dev:Added About content

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -7,17 +7,10 @@ function About() {
         Github Finder
       </h1>
       <p className='mb-4 text-2xl font-light'>
-        A React app to search GitHub profiles and see profile details. This
-        project is part of the
-        <a href='https://www.udemy.com/course/modern-react-front-to-back/'>
-          {' '}
-          React Front To Back
-        </a>{' '}
-        Udemy course by
-        <strong>
-          <a href='https://traversymedia.com'> Brad Traversy</a>
-        </strong>
-        .
+       A React app that can search and display a GitHub profile.
+      </p>
+      <p className='mb-4 text-2xl font-light'>
+        Built in <strong className='text-green-600	'>Tailwind,Daisy UI and React</strong>
       </p>
       
     </div>
