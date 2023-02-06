da1c75f7d4f46638db6c1a6e9b540040014913d8
Emmanuel Koech <93251478+Emmanuel687@users.noreply.github.com>
2023-02-06 21:45:57 +0300

    Dev:Created user sections

--- src/component/layout/users/UserResults.jsx
+++ src/component/layout/users/UserResults.jsx
@@ -3,25 +3,31 @@ import { useEffect, useState } from "react";
 
 function UserResults() {
   const [users, setUsers] = useState();
-  const[loading,setloading]= useState(true);
+  const [loading, setloading] = useState(true);
 
   useEffect(() => {
     fetchUsers();
   }, []);
 
   const fetchUsers = async () => {
-    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
-      headers: {
-        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-      },
-    });
+    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`);
 
     const data = await response.json();
     setUsers(data);
-    setloading(false)
+    setloading(false);
   };
 
-  return <div>UserResults</div>;
+  if(!loading){
+    return (
+        <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+            {users.map((user)=>(<h3>{user.login}</h3>))}
+        </div>
+      );
+
+  }else{
+    return <h3>Loading...</h3>
+  }
+
 }
 
 export default UserResults;
