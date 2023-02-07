718dc986d540f67b9736fa715fe73b31f9e9b25b
Emmanuel Koech <93251478+Emmanuel687@users.noreply.github.com>
2023-02-07 13:06:51 +0300

    Created UserItems

--- src/component/layout/users/UserResults.jsx
+++ src/component/layout/users/UserResults.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import Spinner from "../Spinner";
+import UserItem from "./UserItem";
 
 function UserResults() {
   const [users, setUsers] = useState();
@@ -22,7 +23,7 @@ function UserResults() {
   if(!loading){
     return (
         <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
-            {users.map((user)=>(<h3>{user.login}</h3>))}
+            {users.map((user)=>(<UserItem key={user.id}  user={user}/>))}
         </div>
       );
 
