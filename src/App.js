76473a67b3727d6d9d280c9a4f3d3743ad88733f
Emmanuel Koech <93251478+Emmanuel687@users.noreply.github.com>
2023-02-07 14:00:37 +0300

    Dev:-Created Github context and imported it in App.js

--- src/App.js
+++ src/App.js
@@ -4,24 +4,25 @@ import About from "./Pages/About";
 import NotFound from "./Pages/NotFound";
 import Navbar from "./component/layout/Navbar";
 import Footer from "./component/layout/Footer";
+import { GithubProvider } from "./context/github/GithubContext";
 
 function App() {
   return (
-    <Router>
-      <div className="flex flex-col justify-between h-screen">
-        <Navbar></Navbar>
+    <GithubProvider>
+      <Router>
+        <div className="flex flex-col justify-between h-screen">
+          <Navbar></Navbar>
 
-        <Routes>
-          <Route path="/" element={<Home></Home>}></Route>
-          <Route path="/about" element={<About></About>}></Route>
-          <Route path="/notfound" element={<NotFound></NotFound>}></Route>
-          <Route path="/*" element={<NotFound></NotFound>}></Route>
-
-        </Routes>
-        <Footer></Footer>
-
-      </div>
-    </Router>
+          <Routes>
+            <Route path="/" element={<Home></Home>}></Route>
+            <Route path="/about" element={<About></About>}></Route>
+            <Route path="/notfound" element={<NotFound></NotFound>}></Route>
+            <Route path="/*" element={<NotFound></NotFound>}></Route>
+          </Routes>
+          <Footer></Footer>
+        </div>
+      </Router>
+    </GithubProvider>
   );
 }
 
--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import { createContext } from "react";
+import { useState, useContext } from "react";
 
-function GithubContext() {
-  return (
-    <div>GithubContext</div>
-  )
-}
+const GithubContext = createContext();
+const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
+const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
+
+export const GithubProvider = ({ children }) => {
+  const [users, setUsers] = useState();
+  const [loading, setloading] = useState(true);
+
+  const fetchUsers = async () => {
+    const response = await fetch(`${GITHUB_URL}/users`, {
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`,
+      },
+    });
+
+    const data = await response.json();
+    setUsers(data);
+    setloading(false);
+  };
+
+  return <GithubContext.Provider values={{
+    users,
+    loading,
+  }}>
+    {children}
+  </GithubContext.Provider>
+};
 
 export default GithubContext
